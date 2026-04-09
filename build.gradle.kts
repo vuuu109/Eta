2fbef13c15698ae5aeb2e2c8ae56ed7c1ea57fbd
蛮吉 <hanabi.almost@gmail.com>
2026-04-09 13:36:24 +0800

    build: 升级 AGP、Gradle、Kotlin 和 libxposed 版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.property("kotlinGradlePluginVersion")}")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
 }
